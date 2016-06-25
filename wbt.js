27f8dd9248ff6beea525c90f1d3f9197aada22b5
m890123 <gimmeproxy@ya.ru>
2016-06-25 19:07:21 +0300

    Add files via upload

---
+++ wbt.js
@@ -0,0 +1,51 @@
+$(function() {
+		$( '#wbm-1' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-1' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-1' ).addClass( "active-wbt" );
+			return false;
+		});
+		$( '#wbm-2' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-2' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-2' ).addClass( "active-wbt" );
+			return false;
+		});
+		$( '#wbm-3' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-3' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-3' ).addClass( "active-wbt" );
+			return false;
+		});
+		$( '#wbm-4' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-4' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-4' ).addClass( "active-wbt" );
+			return false;
+		});
+		$( '#wbm-4-1' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-4-1' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-4-1' ).addClass( "active-wbt" );
+			return false;
+		});
+		$( '#wbm-4-2' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-4-2' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-4-2' ).addClass( "active-wbt" );
+			return false;
+		});
+		$( '#wbm-4-3' ).click( function () {
+			$( '.wbm' ).removeClass( "active-wbm" );
+			$( '#wbm-4-3' ).addClass( "active-wbm" );
+			$( '.whatbe-container' ).removeClass( "active-wbt" );
+			$( '#wbt-4-3' ).addClass( "active-wbt" );
+			return false;
+		});
+});
